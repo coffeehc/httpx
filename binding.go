2cec2ea644ba3c15e415246bfae3aabc5eef2cc3
coffee <coffeehc@gmail.com>
2022-08-06 15:44:41 +0800

    更新依赖

--- binding.go
+++ binding.go
@@ -1,79 +1,79 @@
 package httpx
 
 import (
-  "bytes"
-  "fmt"
-  "io"
-  "io/ioutil"
-  "net/http"
+	"bytes"
+	"fmt"
+	"google.golang.org/protobuf/proto"
+	"io"
+	"io/ioutil"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
-  "github.com/gin-gonic/gin/binding"
-  "github.com/gogo/protobuf/proto"
-  "github.com/json-iterator/go"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/json-iterator/go"
 )
 
 var (
-  BindingProtoBuf      = protobufBinding{}
-  BindingJSON          = jsonBinding{}
-  BindingXML           = binding.XML
-  BindingForm          = binding.Form
-  BindingQuery         = binding.Query
-  BindingFormPost      = binding.FormPost
-  BindingFormMultipart = binding.FormMultipart
-  BindingMsgPack       = binding.MsgPack
-  BindingYAML          = binding.YAML
-  BindingUri           = binding.Uri
+	BindingProtoBuf      = protobufBinding{}
+	BindingJSON          = jsonBinding{}
+	BindingXML           = binding.XML
+	BindingForm          = binding.Form
+	BindingQuery         = binding.Query
+	BindingFormPost      = binding.FormPost
+	BindingFormMultipart = binding.FormMultipart
+	BindingMsgPack       = binding.MsgPack
+	BindingYAML          = binding.YAML
+	BindingUri           = binding.Uri
 )
 
 func Bind(c *gin.Context, obj interface{}) error {
-  return c.ShouldBindWith(obj, GetBinding(c))
+	return c.ShouldBindWith(obj, GetBinding(c))
 }
 
 func GetBinding(c *gin.Context) binding.Binding {
-  method, contentType := c.Request.Method, c.ContentType()
-  if method == "GET" {
-    return BindingForm
-  }
+	method, contentType := c.Request.Method, c.ContentType()
+	if method == "GET" {
+		return BindingForm
+	}
 
-  switch contentType {
-  case binding.MIMEJSON:
-    return BindingJSON
-  case binding.MIMEXML, binding.MIMEXML2:
-    return BindingXML
-  case binding.MIMEPROTOBUF:
-    return BindingProtoBuf
-  case binding.MIMEMSGPACK, binding.MIMEMSGPACK2:
-    return BindingMsgPack
-  case binding.MIMEYAML:
-    return BindingYAML
-  default: // case MIMEPOSTForm, MIMEMultipartPOSTForm:
-    return BindingForm
-  }
+	switch contentType {
+	case binding.MIMEJSON:
+		return BindingJSON
+	case binding.MIMEXML, binding.MIMEXML2:
+		return BindingXML
+	case binding.MIMEPROTOBUF:
+		return BindingProtoBuf
+	case binding.MIMEMSGPACK, binding.MIMEMSGPACK2:
+		return BindingMsgPack
+	case binding.MIMEYAML:
+		return BindingYAML
+	default: // case MIMEPOSTForm, MIMEMultipartPOSTForm:
+		return BindingForm
+	}
 }
 
 type protobufBinding struct{}
 
 func (protobufBinding) Name() string {
-  return "protobuf"
+	return "protobuf"
 }
 
 func (b protobufBinding) Bind(req *http.Request, obj interface{}) error {
-  buf, err := ioutil.ReadAll(req.Body)
-  if err != nil {
-    return err
-  }
-  return b.BindBody(buf, obj)
+	buf, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	return b.BindBody(buf, obj)
 }
 
 func (protobufBinding) BindBody(body []byte, obj interface{}) error {
-  if err := proto.Unmarshal(body, obj.(proto.Message)); err != nil {
-    return err
-  }
-  // Here it's same to return validate(obj), but util now we can't add
-  // `binding:""` to the struct which automatically generate by gen-proto
-  return nil
-  // return validate(obj)
+	if err := proto.Unmarshal(body, obj.(proto.Message)); err != nil {
+		return err
+	}
+	// Here it's same to return validate(obj), but util now we can't add
+	// `binding:""` to the struct which automatically generate by gen-proto
+	return nil
+	// return validate(obj)
 }
 
 // EnableDecoderUseNumber is used to call the UseNumber method on the JSON
@@ -86,34 +86,34 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 type jsonBinding struct{}
 
 func (jsonBinding) Name() string {
-  return "json"
+	return "json"
 }
 
 func (jsonBinding) Bind(req *http.Request, obj interface{}) error {
-  if req == nil || req.Body == nil {
-    return fmt.Errorf("invalid request")
-  }
-  return decodeJSON(req.Body, obj)
+	if req == nil || req.Body == nil {
+		return fmt.Errorf("invalid request")
+	}
+	return decodeJSON(req.Body, obj)
 }
 
 func (jsonBinding) BindBody(body []byte, obj interface{}) error {
-  return decodeJSON(bytes.NewReader(body), obj)
+	return decodeJSON(bytes.NewReader(body), obj)
 }
 
 func decodeJSON(r io.Reader, obj interface{}) error {
-  decoder := json.NewDecoder(r)
-  if EnableDecoderUseNumber {
-    decoder.UseNumber()
-  }
-  if err := decoder.Decode(obj); err != nil {
-    return err
-  }
-  return validate(obj)
+	decoder := json.NewDecoder(r)
+	if EnableDecoderUseNumber {
+		decoder.UseNumber()
+	}
+	if err := decoder.Decode(obj); err != nil {
+		return err
+	}
+	return validate(obj)
 }
 
 func validate(obj interface{}) error {
-  if binding.Validator == nil {
-    return nil
-  }
-  return binding.Validator.ValidateStruct(obj)
+	if binding.Validator == nil {
+		return nil
+	}
+	return binding.Validator.ValidateStruct(obj)
 }
--- go.mod
+++ go.mod
@@ -1,28 +1,31 @@
 module github.com/coffeehc/httpx
 
-go 1.17
+go 1.19
 
 require (
-	github.com/gin-gonic/gin v1.7.4
-	github.com/gogo/protobuf v1.3.2
+	github.com/gin-gonic/gin v1.8.1
 	github.com/json-iterator/go v1.1.12
-	go.uber.org/zap v1.19.1
+	go.uber.org/zap v1.21.0
+	google.golang.org/protobuf v1.28.1
 )
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.13.0 // indirect
-	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/protobuf v1.3.3 // indirect
-	github.com/leodido/go-urn v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/ugorji/go/codec v1.1.7 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
+	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
