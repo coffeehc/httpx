727cfb562bf96ea1349271b579a67ec90e23598a
coffee <coffeehc@gmail.com>
2022-09-27 10:37:59 +0800

    修改了log的引入

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/coffeehc/httpx
 go 1.19
 
 require (
+	github.com/coffeehc/base v1.0.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/json-iterator/go v1.1.12
 	go.uber.org/zap v1.21.0
@@ -10,16 +11,27 @@ require (
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
@@ -27,5 +39,7 @@ require (
 	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- service.go
+++ service.go
@@ -1,103 +1,101 @@
 package httpx
 
 import (
-  "context"
-  "fmt"
-  "net/http"
-  "time"
-
-  "github.com/gin-gonic/gin"
-  "go.uber.org/zap"
+	"context"
+	"fmt"
+	"github.com/coffeehc/base/log"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+	"time"
 )
 
 type Service interface {
-  Start(onShutdown func()) <-chan error
-  Shutdown() error
-  GetGinEngine() *gin.Engine
-  NewRouterGroup(prefix string) *gin.RouterGroup
-  GetServerAddress() string
+	Start(onShutdown func()) <-chan error
+	Shutdown() error
+	GetGinEngine() *gin.Engine
+	NewRouterGroup(prefix string) *gin.RouterGroup
+	GetServerAddress() string
 }
 
-func NewService(name string, config *Config, logger *zap.Logger) Service {
-  logger.Debug(fmt.Sprintf("[%s]HTTP服务器配置", name), zap.Any("http_config", config))
-  gin.DisableConsoleColor()
-  gin.SetMode(gin.ReleaseMode)
-  if config == nil {
-    config = &Config{}
-  }
-  gin.EnableJsonDecoderUseNumber()
-  engine := gin.New()
-  engine.RedirectFixedPath = true
-  l, err := Listen(config.getServerAddr())
-  if err != nil {
-    logger.Error(fmt.Sprintf("[%s]创建HTTP服务失败", name), zap.Error(err))
-    return nil
-  }
-  config.ServerAddr = l.Addr().String()
-  l.Close()
-  server := &http.Server{
-    TLSConfig:    config.TLSConfig,
-    TLSNextProto: config.TLSNextProto,
-    ReadTimeout:  config.getReadTimeout(),
-    WriteTimeout: config.getWriteTimeout(),
-    Addr:         config.getServerAddr(),
-    Handler:      engine,
-  }
-  impl := &serviceImpl{
-    name:   name,
-    config: config,
-    logger: logger,
-    engine: engine,
-    server: server,
-  }
-  return impl
+func NewService(name string, config *Config) Service {
+	log.Debug(fmt.Sprintf("[%s]HTTP服务器配置", name), zap.Any("http_config", config))
+	gin.DisableConsoleColor()
+	gin.SetMode(gin.ReleaseMode)
+	if config == nil {
+		config = &Config{}
+	}
+	gin.EnableJsonDecoderUseNumber()
+	engine := gin.New()
+	engine.RedirectFixedPath = true
+	l, err := Listen(config.getServerAddr())
+	if err != nil {
+		log.Error(fmt.Sprintf("[%s]创建HTTP服务失败", name), zap.Error(err))
+		return nil
+	}
+	config.ServerAddr = l.Addr().String()
+	l.Close()
+	server := &http.Server{
+		TLSConfig:    config.TLSConfig,
+		TLSNextProto: config.TLSNextProto,
+		ReadTimeout:  config.getReadTimeout(),
+		WriteTimeout: config.getWriteTimeout(),
+		Addr:         config.getServerAddr(),
+		Handler:      engine,
+	}
+	impl := &serviceImpl{
+		name:   name,
+		config: config,
+		engine: engine,
+		server: server,
+	}
+	return impl
 }
 
 type serviceImpl struct {
-  name   string
-  config *Config
-  logger *zap.Logger
-  engine *gin.Engine
-  server *http.Server
+	name   string
+	config *Config
+	engine *gin.Engine
+	server *http.Server
 }
 
 func (impl *serviceImpl) NewRouterGroup(prefix string) *gin.RouterGroup {
-  return impl.engine.Group(prefix)
+	return impl.engine.Group(prefix)
 }
 
 func (impl *serviceImpl) GetServerAddress() string {
-  return impl.config.getServerAddr()
+	return impl.config.getServerAddr()
 }
 
 func (impl *serviceImpl) Shutdown() error {
-  ctx, _ := context.WithTimeout(context.TODO(), time.Second*30)
-  err := impl.server.Shutdown(ctx)
-  if err != nil {
-    impl.logger.Error("关闭HttpServer失败", zap.Error(err))
-  }
-  return err
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*30)
+	err := impl.server.Shutdown(ctx)
+	if err != nil {
+		log.Error("关闭HttpServer失败", zap.Error(err))
+	}
+	return err
 }
 
 func (impl *serviceImpl) GetGinEngine() *gin.Engine {
-  return impl.engine
+	return impl.engine
 }
 
 func (impl *serviceImpl) Start(onShutdown func()) <-chan error {
-  errorSign := make(chan error, 1)
-  impl.server.RegisterOnShutdown(func() {
-    impl.logger.Debug(fmt.Sprintf("[%s]HTTP服务器关闭", impl.name))
-    if onShutdown != nil {
-      onShutdown()
-    }
-  })
-  go func() {
-    err := impl.server.ListenAndServe()
-    if err != nil && err != http.ErrServerClosed {
-      impl.logger.Error(fmt.Sprintf("[%s]HTTP服务异常关闭", impl.name), zap.Error(err))
-    }
-    impl.logger.Debug(fmt.Sprintf("[%s]HTTP服务关闭", impl.name))
-    errorSign <- err
-  }()
-  impl.logger.Debug(fmt.Sprintf("[%s]HTTP服务启动", impl.name), zap.String("address", impl.config.getServerAddr()))
-  return errorSign
+	errorSign := make(chan error, 1)
+	impl.server.RegisterOnShutdown(func() {
+		log.Debug(fmt.Sprintf("[%s]HTTP服务器关闭", impl.name))
+		if onShutdown != nil {
+			onShutdown()
+		}
+	})
+	go func() {
+		err := impl.server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Error(fmt.Sprintf("[%s]HTTP服务异常关闭", impl.name), zap.Error(err))
+		}
+		log.Debug(fmt.Sprintf("[%s]HTTP服务关闭", impl.name))
+		errorSign <- err
+	}()
+	log.Debug(fmt.Sprintf("[%s]HTTP服务启动", impl.name), zap.String("address", impl.config.getServerAddr()))
+	return errorSign
 }
